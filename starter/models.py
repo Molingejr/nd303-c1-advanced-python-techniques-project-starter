0709bf5db2d676f3b15831603b64b53fa239cf3e
Molingejr <molingejr@gmail.com>
2020-02-29 18:03:56 +0100

    Feature: [Objects Design] Design NearEarthObject and OrbitalPath

--- starter/models.py
+++ starter/models.py
@@ -10,6 +10,12 @@ class NearEarthObject(object):
         :param kwargs:    dict of attributes about a given Near Earth Object, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.orbits = []
+        self.name = kwargs.get('name')
+        self.id = kwargs.get('id')
+        self.is_potentially_hazardous_asteroid = kwargs.get('is_potentially_hazardous_asteroid', False)
+        self.diameter_min_km = float(kwargs.get('estimated_diameter_min_kilometers', 0))
+        self.diameter_max_km = float(kwargs.get('estimated_diameter_max_kilometers', 0))
 
     def update_orbits(self, orbit):
         """
@@ -20,6 +26,15 @@ class NearEarthObject(object):
         """
 
         # TODO: How do we connect orbits back to the Near Earth Object?
+        self.orbits.append(orbit)
+
+    def __repr__(self):
+        """Output Near Earth Object"""
+        return f'NearEarthObject \
+            id:{self.id} \
+            name:{self.name} \
+            orbits: {[orbit.neo_name for orbit in self.orbits]} \
+            orbit_dates:{[orbit.close_approach_date for orbit in self.orbits]}'
 
 
 class OrbitPath(object):
@@ -34,3 +49,13 @@ class OrbitPath(object):
         :param kwargs:    dict of attributes about a given orbit, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.neo_name = kwargs.get('name')
+        self.close_approach_date = kwargs.get('close_approach_date')
+        self.miss_distance_kilometers = float(kwargs.get('miss_distance_kilometers', 0))
+
+    def __repr__(self):
+        """Output Orbit Path"""
+        return f'OrbitPath  \
+            name:{self.neo_name}  \
+            miss_distance_km:{self.miss_distance_kilometers} \
+            orbit_date:{self.close_approach_date}'
